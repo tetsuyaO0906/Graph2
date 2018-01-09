9d373af30cc9e8a75bee6fe08b2bed5b218c1eed
50616008 <50616008@50616008-pc>
2018-01-09 11:12:31 +0900

    homework

---
+++ src/graph2/Graph2_main.java
@@ -0,0 +1,12 @@
+package graph2;
+
+public class Graph2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Graph2_view graph = new Graph2_view();
+		graph.setBounds(5, 5, 655, 455);
+		graph.setVisible(true);
+	}
+
+}
